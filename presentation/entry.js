d1b194c831ba350c7199a25c2ac9ebd45c619482
Spenser Jones <hello@spenserj.com>
2015-11-15 11:52:43 -0700

    Initial commit of rendering slides from Jade with webpack

---
+++ presentation/entry.js
@@ -0,0 +1,5 @@
+'use strict';
+
+import slides from './slides.jade';
+
+document.querySelector('.reveal .slides').innerHTML = slides;
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+'use strict';
+var webpack = require('webpack');
+
+var config = {
+  entry: [
+    './presentation/entry.js',
+    'webpack/hot/dev-server',
+    'webpack-dev-server/client',
+  ],
+  output: {
+    path: __dirname + '/dist',
+    filename: 'presentation.js',
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loaders: ['babel'],
+      },
+      {
+        test: /\.jade$/,
+        exclude: /(node_modules)/,
+        loaders: ['html', 'jade-html'],
+      },
+    ],
+  },
+  devtool: 'eval-source-map',
+};
+
+module.exports = config;
