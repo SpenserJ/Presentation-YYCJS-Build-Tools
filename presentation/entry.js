d1706f553cad1e4cb6e9d15eecaf73bc2d2c5c3b
Spenser Jones <hello@spenserj.com>
2015-11-15 14:04:15 -0700

    Update the slideshow entry to use imports everywhere

--- presentation/entry.js
+++ presentation/entry.js
@@ -3,11 +3,11 @@
 import slides from './slides.jade';
 
 import reveal from 'reveal';
-require('reveal/index.css');
-require('reveal/theme/night.css');
+import 'reveal/index.css';
+import 'reveal/theme/night.css';
 
 import hljs from 'highlight.js';
-require('highlight.js/styles/tomorrow-night.css');
+import 'highlight.js/styles/tomorrow-night.css';
 
 document.querySelector('.reveal .slides').innerHTML = slides;
 
@@ -15,9 +15,7 @@ reveal.initialize({
     controls: true,
     progress: true,
     history: true,
-    center: false,
-    theme: 'night', // available themes are in /css/theme
-    transition: 'linear' // default/cube/page/concave/zoom/linear/fade/none
+    transition: 'linear',
 });
 
 hljs.initHighlightingOnLoad();
